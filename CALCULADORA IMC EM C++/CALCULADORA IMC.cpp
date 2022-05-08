6814b1059d89cfedb3e0f6beebbef948bcfb2c0c
KevenDev <glevenkeven@gmail.com>
2022-05-08 20:03:49 -0300

    CALCULADORA DE IMC EM C++

--- CALCULADORA IMC EM C++/CALCULADORA IMC.cpp
+++ CALCULADORA IMC EM C++/CALCULADORA IMC.cpp
@@ -9,8 +9,29 @@ main() 	{
 	
 	cout<<"Digite sua altura :";
 	cin>>altura;
+	
 	cout<<"Digite seu peso :";
 	cin>>peso;
+	
 	imc = peso/(altura*altura);
-	cout<<"imc = "<<imc<<endl;
+	cout<<"Indice de Massa Corporal = "<<imc<<endl;
+	
+	if(imc<18.5) {
+	
+	cout<<"Obs:abaixo do peso";
+}
+
+	if (imc>=18.5 && imc<24)  {
+	cout<<"Obs:peso normal";
+	
+	}
+	
+	if(imc>=24 && imc<29)
+    {
+      cout<< "Obs:Voce esta acima do peso";
+     }
+  if(imc>29)
+    {
+      cout<< "Obs:voce esta obeso";
+     }
 }
